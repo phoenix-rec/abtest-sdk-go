30609ad4982ced24a3c4d951ba0a3ad8d20a1e7b
lingfeilol <lingfeilol@163.com>
2023-07-04 10:53:40 +0800

    init project

--- abtest/ab_client_test.go
+++ abtest/ab_client_test.go
@@ -12,10 +12,11 @@ func init() {
 	log.InitDefaultLogger()
 	testClient = new(ABClient)
 	hostport := "http://phoenix-rec-recsrv.srv.test.ixiaochuan.cn"
+	//hostport := "http://phoenix-api.ixiaochuan.cn"
 	testClient.Open(testClient, hostport, 10, 16)
 }
 
-func TestServerUnavailable(t *testing.T) {
+func TestServerUnavailable1(t *testing.T) {
 	strategyName, err := testClient.GetStrategyNamesByExpName("recommend_0703")
 	if err != nil {
 		fmt.Printf("get strategy name error: %v\n", err)
@@ -25,3 +26,27 @@ func TestServerUnavailable(t *testing.T) {
 	fmt.Printf("exp info : %v\n", expInfo)
 
 }
+
+//func TestServerUnavailable2(t *testing.T) {
+//	// projectId 项目id,
+//	// hostport  默认实验config域名
+//	// interval  实验config更新间隔，默认为10s
+//	abtest.Open(16, "", 10)
+//
+//	boolVal := abtest.GetBool("123", "exp_name", "key_name", false)
+//	//对照组false  实验组true
+//	if boolVal {
+//
+//	} else {
+//
+//	}
+//
+//	intVal := abtest.GetInt64("123", "exp_name", "key_name", 1)
+//	if intVal == 1 {
+//
+//	} else if intVal == 2 {
+//
+//	} else if intVal == 3 {
+//
+//	}
+//}
